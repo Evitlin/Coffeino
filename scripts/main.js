ab0395848b64d358ccae1c12edb07b60c811515a
Rifle Bird <jumislt@gmail.com>
2025-03-13 20:38:07 +0200

    Dabar tipo veikia viskas ir yra tušti cart, contacts ir t.t. puslapiai 🗿.

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,55 @@
 document.addEventListener('DOMContentLoaded', function() {
+    console.log("DOM fully loaded and parsed");
+    // Load the header from header.html
+    fetch('main_page/header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header-placeholder').innerHTML = data;
+            initializeEventListeners();
+            loadProductLinksScript();
+        })
+        .catch(error => console.error('Error loading header:', error));
+});
+
+function initializeEventListeners() {
+    
     const searchBtn = document.querySelector("#search-btn");
     const searchBox = document.querySelector("#search-box");
     const header = document.querySelector("header");
     const menuBtn = document.querySelector("#menu-btn");
     const closeBtn = document.querySelector("#close-btn");
 
-    // Loads header from header.html
-    fetch('main_page/header.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('header-placeholder').innerHTML = data;
-    });
-    //Toggle menu button
-    menuBtn.addEventListener("click", ()=>{
-        header.classList.toggle("show-mobile-menu");
-    });
-    //Close mobile menu
-    closeBtn.addEventListener("click", ()=>{
-        menuBtn.click();
-    });
+    // Toggle menu button
+    if (menuBtn) {
+        menuBtn.addEventListener("click", () => {
+            header.classList.toggle("show-mobile-menu");
+        });
+    }
+
+    // Close mobile menu
+    if (closeBtn) {
+        closeBtn.addEventListener("click", () => {
+            header.classList.remove("show-mobile-menu");
+        });
+    }
+
     document.getElementById("search-btn").addEventListener("click", function() {
         let searchBox = document.getElementById("search-box");
         searchBox.classList.toggle("active");
     });
-});
+
+}
+
+// Load the product_links.js script after to ensure header and whatever else is loaded
+// otherwise, the script may not find the elements it's looking for :'(
+function loadProductLinksScript() {
+    const script = document.createElement('script');
+    script.src = 'products/product_links.js';
+    script.onload = function() {
+        console.log("product_links.js script loaded and executed");
+    };
+    script.onerror = function() {
+        console.error("Error loading product_links.js script");
+    };
+    document.body.appendChild(script);
+}
