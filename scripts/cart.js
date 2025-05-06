8014df5baa703610db535ce6ec622a0cc9754fe4
agne-pixel <agnemurauskaite1d@gmail.com>
2025-05-06 19:26:38 +0300

    č

--- scripts/cart.js
+++ scripts/cart.js
@@ -29,8 +29,8 @@ firebase.auth().onAuthStateChanged(user => {
                 if (querySnapshot.empty) {
                     // Show empty cart message and keep the shopping bag image
                     cartContainer.innerHTML = `
-                        <div class="form-content">
-                        <img src="/cart/cart_images/shoping_bag.png" alt="Product Image" class="product-image">
+                        <div class="empty-cart-content">
+                        <img src="/cart/cart_images/shoping_bag.png" alt="Empty Cart" class="empty-cart-image">
                         <p class="empty-text">The cart is empty.</p>
                         <a href="/products/product.html" id="shop_button">Go shopping</a>
                         </div>
